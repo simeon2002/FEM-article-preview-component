11f9fc5887a195f546d6e00da6e1a73acb89c055
Simeon <61100437+simeon2002@users.noreply.github.com>
2026-08-01 00:52:32 +0300

    Style share component and add interactivity for tablet/desktop

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ const shareBtn = document.querySelector(".btn--share");
 const footer = shareBtn.closest(".article__footer");
 
 shareBtn.addEventListener("click", e => {
+  console.log(this.innerWidth);
+
   footer.classList.toggle("share--open");
 });
